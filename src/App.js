f5a69e1e6ff7d42609e12020674ad5251ddc22bf
Nwilso21 <noahwilson6381@gmail.com>
2024-07-07 20:36:21 -0600

    Pie Chart Additions

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { InfoAlert } from './components/Alert';
 import { ErrorAlert } from './components/Alert';
 import {WarningAlert} from './components/Alert';
 import CityEventsChart from './components/CityEventsChart';
+import EventGenresChart from './components/EventGenresChart';
 
 import './App.css';
 
@@ -52,7 +53,10 @@ const App = () => {
         setCurrentCity={setCurrentCity}
         setInfoAlert={setInfoAlert} />
       <NumberOfEvents setCurrentNOE={setCurrentNOE} setErrorAlert={setErrorAlert} />
-      <CityEventsChart allLocations={allLocations} events={events} />
+      <div className="charts-container">
+        <EventGenresChart events={events} />
+        <CityEventsChart allLocations={allLocations} events={events} />
+      </div>
       <EventList events={events} />
     </div>
  );
